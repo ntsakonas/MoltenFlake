77ed3c4f68cc2cd4b11e5f56c54c734d4b8c0063
ntsakonas <magicnickus@yahoo.com>
2021-01-28 13:29:54 +0000

    added empty project

---
+++ src/main/java/com/ntsakonas/moltenflake/moltenflake/MoltenFlakeApplication.java
@@ -0,0 +1,13 @@
+package com.ntsakonas.moltenflake.moltenflake;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoltenFlakeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoltenFlakeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ntsakonas/moltenflake/moltenflake/MoltenFlakeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ntsakonas.moltenflake.moltenflake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoltenFlakeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
