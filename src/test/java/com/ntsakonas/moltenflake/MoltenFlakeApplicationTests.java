40555bcb1cf14756322c8e08f8dcc4aea19945f6
ntsakonas <magicnickus@yahoo.com>
2021-01-28 13:33:35 +0000

    removed extra package

--- src/test/java/com/ntsakonas/moltenflake/MoltenFlakeApplicationTests.java
+++ src/test/java/com/ntsakonas/moltenflake/MoltenFlakeApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ntsakonas.moltenflake.moltenflake;
+package com.ntsakonas.moltenflake;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
