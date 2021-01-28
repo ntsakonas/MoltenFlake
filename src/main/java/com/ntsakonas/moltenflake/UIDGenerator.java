d644e1e6ecfe471a0f86bf2777e7d47cc477fe1e
ntsakonas <magicnickus@yahoo.com>
2021-01-28 13:47:44 +0000

    added draft REST api and empty UID generator

---
+++ src/main/java/com/ntsakonas/moltenflake/UIDGenerator.java
@@ -0,0 +1,11 @@
+package com.ntsakonas.moltenflake;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class UIDGenerator {
+
+    public long generateUid() {
+        return 1234;
+    }
+}
